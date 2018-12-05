91c6370f28485c53024c7e59cd8955f11324e80c
Marcelo Gusman <gusmanservicios@hotmail.com>
2018-12-05 16:03:13 -0300

    Update app.js
    se logra efecto en botones

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,4 +1,4 @@
-var num1=0
+/*var num1=0
 var num2=0
 var resultado=0
 
@@ -30,16 +30,29 @@ var Calculadora = (function (num1, num2){
 			return resultado
 		}
 	}
-})
+})*/
 
-var uno = document.getElementById('1');
+var uno = document.getElementById("1");
 // asi para todos los numeros
+uno.addEventListener("click", function(){
+	self.numero("1")
+})
 
 uno.addEventListener("mousedown", function(){
-	document.getElementById("1").style="scale:0.7;"
-	document.getElementById("1").innerHTML="display";
-
+	uno.setAttribute("style", "transform:scale(0.95,0.95)")
 })
+uno.addEventListener("mouseout", function(){
+	uno.setAttribute("style", "transform:scale(1,1)")
+})
+
+
+/*uno.addEventListener("mousedown", numerouno);
+
+function numerouno(){
+	document.getElementById("1").style="scale:0.7;"
+	document.getElementById("1").innerHTML="#display";
+}*/
+
 
 
 
